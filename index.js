60d9f5206af454938430d2fb0a163a1aea038baf
Arr-333 <151920755+Arr-333@users.noreply.github.com>
2024-07-13 16:51:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const io = require("socket.io")(8000, {
+  cors: "*",
+});
+
+let users = {};
+
+io.on("connection", (socket) => {
+  socket.on("joined", (name) => {
+    if (name == "" || name == null || name == "null") {
+    } else {
+      users[socket.id] = name;
+      socket.broadcast.emit("new-user-joined", name);
+    }
+  });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("recieve", {
+      name: users[socket.id],
+      message: message,
+    });
+  });
+  socket.on("disconnect", () => {
+    if (
+      users[socket.id] == "" ||
+      users[socket.id] == null ||
+      users[socket.id] == "null"
+    ) {
+    } else {
+      socket.broadcast.emit("left", users[socket.id]);
+      delete users[socket.id];
+    }
+  });
+});
